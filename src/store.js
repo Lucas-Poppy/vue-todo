75e72ca9de701dd26c1a1fd8d8212e5855d3f7a4
荒木祐輝 <lucaspoppy8080@gmail.com>
2019-08-25 23:43:35 +0900

    カレンダーと連携した

--- src/store.js
+++ src/store.js
@@ -1,12 +1,10 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import test from '@/store/test'
 import todoList from '@/store/todoList'
 
 Vue.use(Vuex)
 export default new Vuex.Store({
   modules: {
-    test: test,
     todoList: todoList
   }
 })
